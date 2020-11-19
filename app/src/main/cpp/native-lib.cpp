422a7edd3c8f99eb7e8309b51e8cad6737687ac2
DESKTOP-PVHN6IV\Admin <1004314574@qq.com>
2020-11-19 19:02:45 +0800

    first commit

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,17 @@
+#include <jni.h>
+#include <string>
+
+
+extern "C"
+{
+    JNIEXPORT jstring
+jstring
+
+Java_com_example_sdl2compile_MainActivity_stringFromJNI(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
+
+}
---
+++ app/src/main/java/com/example/sdl2compile/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.sdl2compile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
